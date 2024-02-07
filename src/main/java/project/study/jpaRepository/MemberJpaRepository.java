4664ea842dbe56b4311b748789c38cb39170b463
김승우 <tmd8633@naver.com>
2024-02-07 14:09:39 +0900

    Fixed : 누락데이터 업로드

--- src/main/java/project/study/jpaRepository/MemberJpaRepository.java
+++ src/main/java/project/study/jpaRepository/MemberJpaRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.study.domain.Member;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByNickname(String nickName);
 }
