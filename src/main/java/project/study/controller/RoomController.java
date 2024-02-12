bf140bcaa4357155832a7cd72a6e10ed74311804
김승우 <tmd8633@naver.com>
2024-02-12 19:54:11 +0900

    Add : 방생성 JS 구현

--- src/main/java/project/study/controller/RoomController.java
+++ src/main/java/project/study/controller/RoomController.java
@@ -2,6 +2,10 @@ package project.study.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import project.study.constant.WebConst;
+
+import static project.study.constant.WebConst.LOGIN_MEMBER;
 
 @Controller
 public class RoomController {
@@ -13,7 +17,8 @@ public class RoomController {
     }
 
     @GetMapping("/")
-    public String main(){
+    public String main(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId){
+        System.out.println("memberId = " + memberId);
         return "main";
     }
 }
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -26,11 +26,7 @@ window.addEventListener('load', () => {
     const modalExitList = document.querySelector('.modal-exit');
     if (modalExitList != null) {
         modalExitList.addEventListener('click', () => {
-            modal.classList.toggle(disabled);
-            setTimeout(() => {
-                modal_content.innerHTML = '';
-                initModalSize();
-            }, 200);
+            modalExit();
         })
     }
 
@@ -113,6 +109,16 @@ window.addEventListener('load', () => {
 
 })
 
+function modalExit() {
+    const modal = document.querySelector('.modal');
+    const modal_content = document.querySelector('.modal-content');
+
+    modal.classList.toggle(disabled);
+    setTimeout(() => {
+        modal_content.innerHTML = '';
+        initModalSize();
+    }, 200);
+}
 function login() {
     let loginAccount = document.querySelector('input[name="loginAccount"]');
     let loginPassword = document.querySelector('input[name="loginPassword"]');
@@ -385,6 +391,18 @@ function changeToFindPassword() {
         modal_content.classList.remove('change');
     }, 800);
 }
+function changeToCreateRoom() {
+    const modal_content = document.querySelector('.modal-content');
+    const modal_wrap = document.querySelector('.modal-wrap');
+    
+    modal_content.classList.add('change');
+    setTimeout(() => {
+        modal_wrap.classList.remove('modal-create-room');
+        modal_content.innerHTML = createCreateRoomModal();
+        modal_wrap.classList.add('modal-create-room');
+        modal_content.classList.remove('change');
+    }, 800);
+}
 
 function changeToSignup() {
     const modal_content = document.querySelector('.modal-content');
@@ -563,7 +581,7 @@ function createCreateRoomModal() {
     return  '<div class="modal-wrapper">' +
                 '<div class="create-room-image-box">' +
                     '<button type="button" id="img">' +
-                        '<img src="/static/image/IMG_8012.jpeg" width="100%" height="100%" id="myProfile">' +
+                        '<img src="/image/basic-room-profile.jpg" width="100%" height="100%" id="roomProfile" name="roomProfile">' +
                         '<input type="file" name="roomImage" id="roomImage" accept="image/*">' +
                         '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" class="g68VV5Ghc0ymGpbFWhEx"><circle cx="18" cy="18" r="18" fill="#000"></circle><path d="M11.375 22.658v2.969h2.969l8.756-8.756-2.97-2.969-8.755 8.756zm14.02-8.083a.788.788 0 000-1.116l-1.852-1.852a.788.788 0 00-1.116 0l-1.45 1.448 2.97 2.97 1.448-1.45z" fill="#fff"></path></svg>' +
                     '</button>' +
@@ -571,18 +589,20 @@ function createCreateRoomModal() {
                 '<div class="input-box">' +
                     '<h4>방 제목</h4>' +
                     '<input type="text" name="title" id="title" maxlength="10">' +
+                    '<span class="msg disabled m-title"></span>' +
                 '</div>' +
                 '<div class="input-box">' +
                     '<h4>소개글</h4>' +
                     '<input type="text" name="intro" id="intro" maxlength="50">' +
+                    '<span class="msg disabled m-intro"></span>' +
                 '</div>' +
                 '<div class="input-wrap">' +
                     '<h4>인원 수' +
-                        '<input type="radio" name="max" id="m2">' +
-                        '<input type="radio" name="max" id="m3">' +
-                        '<input type="radio" name="max" id="m4" checked>' +
-                        '<input type="radio" name="max" id="m5">' +
-                        '<input type="radio" name="max" id="m6">' +
+                        '<input type="radio" name="max" id="m2" value="2">' +
+                        '<input type="radio" name="max" id="m3" value="3">' +
+                        '<input type="radio" name="max" id="m4" value="4" checked>' +
+                        '<input type="radio" name="max" id="m5" value="5">' +
+                        '<input type="radio" name="max" id="m6" value="6">' +
                     '</h4>' +
                     '<div class="radio-wrap">' +
                         '<label for="m2" aria-selected="false">2명</label>' +
@@ -592,29 +612,10 @@ function createCreateRoomModal() {
                         '<label for="m6" aria-selected="false">6명</label>' +
                     '</div>' +
                 '</div>' +
+                '<span class="msg disabled m-max"></span>' +
                 '<div class="input-wrap tag-wrap">' +
                     '<h4>태그</h4>' +
                     '<div class="tag-list">' +
-                        '<div class="tag-box">' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="tagSvg" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>' +
-                            '<span type="text" name="tag" id="" value="백엔드">백엔드</span>' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="xSvg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>' +
-                        '</div>' +
-                        '<div class="tag-box">' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="tagSvg" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>' +
-                            '<span type="text" name="tag" id="" value="백엔드">가나다라마바사아차</span>' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="xSvg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>' +
-                        '</div>' +
-                        '<div class="tag-box">' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="tagSvg" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>' +
-                            '<span type="text" name="tag" id="" value="백엔드">가나다라마바</span>' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="xSvg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>' +
-                        '</div>' +
-                        '<div class="tag-box">' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="tagSvg" viewBox="0 0 448 512"><path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>' +
-                            '<span type="text" name="tag" id="" value="백엔드">아아아아아아아아아아아</span>' +
-                            '<svg xmlns="http://www.w3.org/2000/svg" class="xSvg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>' +
-                        '</div>' +
                         '<div class="tag-add-box">' +
                             '<span>#</span>' +
                             '<input type="text" name="tag-add" id="tag-add" placeholder="태그입력" maxlength="10">' +
