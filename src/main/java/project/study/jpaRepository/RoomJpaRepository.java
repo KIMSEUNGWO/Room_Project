88d02d80cec846203fec55db2b09746f26f05a95
김승우 <tmd8633@naver.com>
2024-02-13 14:58:04 +0900

    Refactor : repository 이름 변경

--- src/main/java/project/study/jpaRepository/RoomJpaRepository.java
+++ src/main/java/project/study/jpaRepository/RoomJpaRepository.java
@@ -3,6 +3,5 @@ package project.study.jpaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.study.domain.Room;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
+public interface RoomJpaRepository extends JpaRepository<Room, Long> {
 }
