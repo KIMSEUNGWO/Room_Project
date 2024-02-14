cee88ff6c895da024d908ac7b5e7d33860ca0fe9
seungchan <starosc37@gmail.com>
2024-02-14 19:11:37 +0900

    Add : 인터셉터 추가

--- src/main/java/project/study/config/WebConfig.java
+++ src/main/java/project/study/config/WebConfig.java
@@ -10,9 +10,11 @@ import org.springframework.util.unit.DataSize;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.study.customAnnotation.argumentresolver.PathRoomArgumentResolver;
 import project.study.customAnnotation.argumentresolver.SessionLoginArgumentResolver;
+import project.study.intercept.LoginInterceptor;
 import project.study.jpaRepository.MemberJpaRepository;
 import project.study.jpaRepository.RoomJpaRepository;
 
@@ -38,5 +40,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SessionLoginArgumentResolver(memberJpaRepository));
         resolvers.add(new PathRoomArgumentResolver(roomJpaRepository));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+            .order(1)
+            .addPathPatterns("")
+            .excludePathPatterns("/resource/js/**", "/resource/css/**");
     }
 }
