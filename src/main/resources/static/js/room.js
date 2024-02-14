8a167e50bd76f10bf8fe041f6e2d09db74f32c7b
seungchan <starosc37@gmail.com>
2024-02-14 14:46:01 +0900

    Add : html파일 static제거

--- src/main/resources/static/js/room.js
+++ src/main/resources/static/js/room.js
@@ -18,6 +18,7 @@ window.addEventListener('load', () => {
 
     roomSettingMenuOpen();
     roomSettingMenuClose();
+    editRoomModalOpen();
 
 });
 
@@ -87,6 +88,18 @@ function notifyModalOpen(){
     notifyBox.addEventListener('click', function(){
         insertModalSize('modal-notify');
         modal_content.innerHTML = createNotify();
+        modal.classList.remove('disabled');
+    });
+};
+
+function editRoomModalOpen() {
+    let roomEditModal = document.querySelector('.setting-menu');
+    const modal = document.querySelector('.modal');
+    const modal_content = document.querySelector('.modal-content');
+
+    roomEditModal.addEventListener('click', function(){
+        insertModalSize('modal-edit-room');
+        modal_content.innerHTML = editRoomModal();
         modal.classList.remove('disabled');
     });
 };
@@ -162,5 +175,72 @@ function createNotify() {
                     '<button type="button" id="do-notify">신고하기</button>' +
                     '<button type="button" id="cancel-notify">취소</button>' +
                 '</div>' +
+            '</div>';
+};
+
+function editRoomModal() {
+    return  '<div class="modal-wrapper">' +
+                '<div class="create-room-image-box">' +
+                    '<button type="button" id="img">' +
+                        '<img src="/images/room_profile/basic-room-profile.jpg" width="100%" height="100%" id="roomProfile" name="roomProfile">' +
+                        '<input type="file" name="roomImage" id="roomImage" accept="image/*">' +
+                        '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" class="g68VV5Ghc0ymGpbFWhEx"><circle cx="18" cy="18" r="18" fill="#000"></circle><path d="M11.375 22.658v2.969h2.969l8.756-8.756-2.97-2.969-8.755 8.756zm14.02-8.083a.788.788 0 000-1.116l-1.852-1.852a.788.788 0 00-1.116 0l-1.45 1.448 2.97 2.97 1.448-1.45z" fill="#fff"></path></svg>' +
+                    '</button>' +
+                '</div>' +
+                '<div class="input-box">' +
+                    '<h4>방 제목</h4>' +
+                    '<input type="text" name="title" id="title" maxlength="10">' +
+                    '<span class="msg disabled m-title"></span>' +
+                '</div>' +
+                '<div class="input-box">' +
+                    '<h4>소개글</h4>' +
+                    '<input type="text" name="intro" id="intro" maxlength="50">' +
+                    '<span class="msg disabled m-intro"></span>' +
+                '</div>' +
+                '<div class="input-wrap">' +
+                    '<h4>인원 수' +
+                        '<input type="radio" name="max" id="m2" value="2">' +
+                        '<input type="radio" name="max" id="m3" value="3">' +
+                        '<input type="radio" name="max" id="m4" value="4" checked>' +
+                        '<input type="radio" name="max" id="m5" value="5">' +
+                        '<input type="radio" name="max" id="m6" value="6">' +
+                    '</h4>' +
+                    '<div class="radio-wrap max-wrap">' +
+                        '<label for="m2" aria-selected="false">2명</label>' +
+                        '<label for="m3" aria-selected="false">3명</label>' +
+                        '<label for="m4" aria-selected="true">4명</label>' +
+                        '<label for="m5" aria-selected="false">5명</label>' +
+                        '<label for="m6" aria-selected="false">6명</label>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="input-wrap">' +
+                    '<h4>공개여부' +
+                        '<input type="radio" name="public" id="public" value="PUBLIC" checked>' +
+                        '<input type="radio" name="public" id="private" value="PRIVATE">' +
+                    '</h4>' +
+                    '<div class="radio-wrap">' +
+                        '<label for="public" aria-selected="true">공개방</label>' +
+                        '<label for="private" aria-selected="false">비공개방</label>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="input-box password-box disabled">' +
+                    '<h4>비밀번호 설정</h4>' +
+                    '<input type="password" name="room-password" id="room-password" placeholder="비밀번호 4~6자리를 설정해주세요." minlength="4" maxlength="6">' +
+                    '<span class="msg disabled m-private-password"></span>' +
+                '</div>' +
+                '<span class="msg disabled m-max"></span>' +
+                '<div class="input-wrap tag-wrap">' +
+                    '<h4>태그</h4>' +
+                    '<div class="tag-list">' +
+                        '<div class="tag-add-box">' +
+                            '<span>#</span>' +
+                            '<input type="text" name="tag-add" id="tag-add" placeholder="태그입력" maxlength="10">' +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+            '<div class="buttons">' +
+                '<button type="button" id="room-cancel">취소</button>' +
+                '<button type="button" id="room-edit">저장</button>' +
             '</div>';
 };
