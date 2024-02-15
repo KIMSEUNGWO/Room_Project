018be6690ddbee177c98a515988a7d5aee9eb41a
seungchan <starosc37@gmail.com>
2024-02-15 18:49:24 +0900

    Add : Admin 이용중인 회원 페이지 기능 구현 중

---
+++ src/main/java/project/study/dto/admin/AdminMembersDto.java
@@ -0,0 +1,43 @@
+package project.study.dto.admin;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import project.study.domain.Phone;
+import project.study.domain.Social;
+import project.study.enums.MemberStatusEnum;
+import project.study.enums.SocialEnum;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class AdminMembersDto {
+
+    private String account;
+    private String socialEmail;
+    private String memberName;
+    private String memberNickname;
+    private String phone;
+    private LocalDateTime memberCreateDate;
+    private int memberNotifyCount;
+    private SocialEnum socialType;
+    private MemberStatusEnum memberStatusEnum;
+
+    @QueryProjection
+    public AdminMembersDto(String account, String socialEmail, String memberName, String memberNickname, String phone, LocalDateTime memberCreateDate, int memberNotifyCount, SocialEnum socialType, MemberStatusEnum memberStatusEnum) {
+        this.account = account;
+        this.socialEmail = socialEmail;
+        this.memberName = memberName;
+        this.memberNickname = memberNickname;
+        this.phone = phone;
+        this.memberCreateDate = memberCreateDate;
+        this.memberNotifyCount = memberNotifyCount;
+        this.socialType = socialType;
+        this.memberStatusEnum = memberStatusEnum;
+    }
+}
