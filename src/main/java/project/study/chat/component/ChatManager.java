9a273c1f65cca65fcb5954e1a84cde744116e53c
KIMSEUNGWOO <tmd8633@naver.com>
2024-03-15 15:58:02 +0900

    Refactor : 코드정리

---
+++ src/main/java/project/study/chat/component/ChatManager.java
@@ -0,0 +1,22 @@
+package project.study.chat.component;
+
+import project.study.chat.MessageType;
+import project.study.chat.dto.ChatDto;
+import project.study.domain.Member;
+import project.study.domain.Room;
+
+import java.time.LocalDateTime;
+
+public class ChatManager {
+
+    public static ChatDto sendMessageChatDto(Member member, Room room, MessageType messageType, String message) {
+        return ChatDto.builder()
+                .roomId(room.getRoomId())
+                .type(messageType)
+                .sender(member.getMemberNickname())
+                .time(LocalDateTime.now())
+                .token(member.getMemberId())
+                .message(message)
+                .build();
+    }
+}
