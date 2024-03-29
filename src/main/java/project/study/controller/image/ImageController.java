e1828376bdc4e48265abb2a55ac7ca2bac254559
KIMSEUNGWOO <tmd8633@naver.com>
2024-03-29 13:34:53 +0900

    Add :amazonS3 구현중

--- src/main/java/project/study/controller/image/ImageController.java
+++ src/main/java/project/study/controller/image/ImageController.java
@@ -1,5 +1,6 @@
 package project.study.controller.image;
 
+import com.amazonaws.services.s3.AmazonS3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -16,15 +17,20 @@ public class ImageController {
 
     @Value("${file.dir}")
     private String fileDir;
+    @Value("${cloud.aws.s3.bucketName}")
+    private String bucketName; //버킷 이름
+    private final AmazonS3 amazonS3;
 
     @GetMapping(value = "/images/{fileUploadType}/{filename}", produces = {"image/png", "image/jpg", "image/jpeg"})
     public Resource downloadImage1(@PathVariable(name = "fileUploadType") String fileUploadType, @PathVariable(name = "filename") String filename) throws MalformedURLException {
         FileUploadType type = FileUploadType.findDir(fileUploadType);
-        return new UrlResource("file:" + getFullPath(filename, type));
+//        return new UrlResource("file:" + getFullPath(filename, type));
+        return new UrlResource("file:" + amazonS3.getUrl(bucketName, getFullPathS3(filename, type)));
     }
     @GetMapping("/images/{filename}")
     public Resource downloadImage2(@PathVariable(name = "filename") String filename) throws MalformedURLException {
-        return new UrlResource("file:" + getFullPath(filename, null));
+//        return new UrlResource("file:" + getFullPath(filename, null));
+        return new UrlResource("file:" + amazonS3.getUrl(bucketName, getFullPathS3(filename, null)));
     }
 
     private String getFullPath(String fileName, FileUploadType type) {
@@ -33,6 +39,15 @@ public class ImageController {
         if (type == null) return sb.append("/").append(fileName).toString();
 
         String folderName = type.getDir();
+        return sb.append(folderName).append("/").append(fileName).toString();
+    }
+
+    private String getFullPathS3(String fileName, FileUploadType fileType) {
+        StringBuilder sb = new StringBuilder("/");
+
+        if (fileType == null) return sb.append(fileName).toString();
+
+        String folderName = fileType.getDir();
         return sb.append(folderName).append("/").append(fileName).toString();
     }
 }
